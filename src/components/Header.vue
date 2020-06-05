7551b6d3db5b8f106725140cf4c4386d362b53aa
zkk-pro <yejinxiu@hexinquandeMacBook-Pro.local>
2020-06-05 19:15:30 +0800

    response

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,11 @@
 <template>
   <q-header :elevated="false" class="row justify-center bg-dark">
     <div class="header-box row items-center justify-between col-xs-11">
+      <div class="gt-xs">123</div>
       <q-icon
         name="img:statics/icons/menu-up.png"
         size="18px"
-        class="left-menu"
+        class="left-menu lt-sm"
       ></q-icon>
 
       <!-- 左侧菜单 -->
@@ -14,7 +15,7 @@
         </template>
       </Menu>
 
-      <div class="header-title absolute-center">RBI</div>
+      <div class="header-title absolute-center lt-sm">RBI</div>
       <div class="row items-center">
         <q-icon
           v-if="$q.lang.isoName === 'en-us'"
@@ -29,10 +30,15 @@
         <q-icon
           name="img:statics/icons/account.png"
           size="18px"
-          class="account"
+          class="account lt-sm"
           style="margin-left:20px"
         >
         </q-icon>
+        <div class="gt-xs row " style="text-white; margin-left: 40px">
+          <router-link to="/registry">注册</router-link>
+          <span class="q-px-md">|</span>
+          <router-link to="/login">登录</router-link>
+        </div>
 
         <!-- 个人中心icon 菜单栏 -->
         <Menu :menuData="accountMenu" target=".account">
@@ -92,7 +98,12 @@ export default {
 
 <style lang="scss" scoped>
 .header-box {
-  height: 44px;
+  height: 60px;
+}
+.screen--xs {
+  .header-box {
+    height: 44px;
+  }
 }
 .header-title {
   font-size: 18px;
--- src/pages/Login/Index.vue
+++ src/pages/Login/Index.vue
@@ -42,7 +42,7 @@
     <q-list>
       <q-item class="q-mt-md">
         <q-item-section class="text-right text-primary q-mr-lg">
-          <router-link to="/">忘记密码</router-link>
+          <span class="cursor-pointer" @click="forgetPwd">忘记密码</span>
         </q-item-section>
         <q-separator inset vertical class="bg-primary" />
         <q-item-section class="text-primary q-ml-lg">
@@ -50,10 +50,77 @@
         </q-item-section>
       </q-item>
     </q-list>
+
+    <!-- 忘了密码一系列弹框 -->
+    <!-- 确认框 -->
+    <Dialog title="重置登录密码" ref="resetTip" @confirm="resetTipCofirm">
+      <div class="q-pl-md" style="font-size:14px; color: #666">
+        重置登录密码后，24小时内无法提币
+      </div>
+    </Dialog>
+    <!-- 账号输入 -->
+    <Dialog
+      title="重置登录密码"
+      ref="resetInput"
+      confirmHold
+      @confirm="resetInputCofirm"
+      @hide="forgetAccount = ''"
+    >
+      <q-input
+        class="q-px-md"
+        v-model="forgetAccount"
+        autofocus
+        label="请输入邮箱或手机号"
+        lazy-rules
+        no-error-icon
+        :error="resetInputErr && !forgetAccount.length"
+        error-message="请输入邮箱或手机号"
+      />
+    </Dialog>
+    <!-- 安全验证 -->
+    <SafeValidate
+      :show.sync="safeShow"
+      :validType="securityLevel"
+      @safeConfirm="onSafeConfirm"
+    />
+    <!-- 设置新的密码 -->
+    <Dialog
+      title="设置新登录密码"
+      ref="resetSetPwd"
+      confirmHold
+      @confirm="resetSetPwdCofirm"
+      @hide=";[(newPwdForm.first = ''), (newPwdForm.second = '')]"
+    >
+      <q-form ref="newPwdForm" class="q-px-md">
+        <q-input
+          v-model="newPwdForm.first"
+          type="password"
+          dense
+          label="请设置登录密码"
+          lazy-rules
+          no-error-icon
+          :rules="[
+            val => !!val || '请设置登录密码',
+            val => !(val.length < 6) || '密码不能小于6位数'
+          ]"
+        />
+        <q-input
+          v-model="newPwdForm.second"
+          type="password"
+          dense
+          label="请再次输入登录密码"
+          lazy-rules
+          no-error-icon
+          :rules="[val => val === newPwdForm.first || '两次输入不一致']"
+        />
+      </q-form>
+    </Dialog>
   </q-page>
 </template>
 
 <script>
+import Dialog from 'components/Dialog'
+import { getAccountSafeLevel, resetPwd } from 'src/api/apiList'
 export default {
   data() {
     return {
@@ -62,14 +129,76 @@ export default {
         password: ''
       },
       pwdVisible: false,
-      fromPath: '/'
+      fromPath: '/',
+      resetInputErr: false,
+      forgetAccount: '',
+      securityLevel: 1, // 安全验证类别
+      safeShow: false,
+      resetPwdParams: {
+        account: '',
+        validCode: ''
+      },
+      newPwdForm: {
+        first: '',
+        second: ''
+      }
     }
   },
-  beforeRouteLeave(to, from, next) {
-    this.fromPath = from.path
-    next()
+  components: { Dialog },
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.fromPath = from.path
+    })
   },
   methods: {
+    // 忘记密码
+    forgetPwd() {
+      this.$refs.resetTip.open()
+    },
+    // 忘记密码确认弹框 confirm
+    resetTipCofirm() {
+      this.$refs.resetInput.open()
+    },
+    // 输入手机号/邮箱
+    async resetInputCofirm() {
+      if (!this.forgetAccount) {
+        return (this.resetInputErr = true)
+      }
+      try {
+        const { data } = await getAccountSafeLevel({
+          account: this.forgetAccount
+        })
+        this.securityLevel = data.securityLevel
+        this.resetPwdParams.account = this.forgetAccount
+        this.$refs.resetInput.close()
+        this.safeShow = true
+      } catch (error) {}
+    },
+    // 安全验证
+    onSafeConfirm(code) {
+      this.resetPwdParams.validCode = code
+      this.safeShow = false
+      this.$refs.resetSetPwd.open()
+    },
+    // 设置新密码
+    async resetSetPwdCofirm() {
+      const res = await this.$refs.newPwdForm.validate()
+      if (res) {
+        try {
+          await resetPwd({
+            account: this.resetPwdParams.account,
+            password: this.newPwdForm.first,
+            code: this.resetPwdParams.validCode
+          })
+          this.$q.notify({
+            message: '修改成功',
+            icon: 'done',
+            textColor: 'green'
+          })
+          this.$refs.resetSetPwd.close()
+        } catch (error) {}
+      }
+    },
     async onSubmit() {
       const res = await this.$refs.login.validate()
       if (res) {
